0eae9d07df5018ee377b5b7ca63cc870310c0fe7
Jéssica Cardoso <jessica.cardosor@gmail.com>
2024-12-03 22:33:11 -0300

    Criação ResultViewModel para padrão de respostas.

---
+++ Application/Models/ResultViewModel.cs
@@ -0,0 +1,45 @@
+﻿namespace LogTransformer.Application.Models
+{
+    public class ResultViewModel
+    {
+        public ResultViewModel(bool isSuccess = true, string message = "")
+        {
+            IsSuccess = isSuccess;
+            Message = message;
+        }
+
+        public bool IsSuccess { get; private set; }
+        public string Message { get; private set; }
+
+        public static ResultViewModel Success()
+        {
+            return new ResultViewModel(true, string.Empty);
+        }
+
+        public static ResultViewModel Error(string message)
+        {
+            return new ResultViewModel(false, message);
+        }
+    }
+
+    public class ResultViewModel<T> : ResultViewModel
+    {
+        public ResultViewModel(T data, bool isSuccess = true, string message = "")
+            : base(isSuccess, message)
+        {
+            Data = data;
+        }
+
+        public T Data { get; private set; }
+
+        public static ResultViewModel<T> Success(T data)
+        {
+            return new ResultViewModel<T>(data, true, string.Empty);
+        }
+
+        public static ResultViewModel<T> Error(string message)
+        {
+            return new ResultViewModel<T>(default, false, message);
+        }
+    }
+}
