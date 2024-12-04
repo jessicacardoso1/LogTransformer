b43bc7c90c3437eddf1c939d50ac29d6bf90901a
Jéssica Cardoso <jessica.cardosor@gmail.com>
2024-12-03 22:28:08 -0300

    Criação LogEntry.cs

---
+++ Core/Entities/LogEntry.cs
@@ -0,0 +1,17 @@
+﻿using DevFreela.Core.Entities;
+using System;
+
+namespace LogTransformer.Core.Entities
+{
+    public class LogEntry : BaseEntity
+    {
+        public string Provider { get; set; } = "MINHA CDN";
+        public string HttpMethod { get; set; }
+        public int StatusCode { get; set; }
+        public string UriPath { get; set; }
+        public double TimeTaken { get; set; }
+        public int ResponseSize { get; set; }
+        public string CacheStatus { get; set; }
+    }
+
+}
