3c2fd6f8e83dea9e79ca4c486d833d4c496701d6
Jéssica Cardoso <jessica.cardosor@gmail.com>
2024-12-03 22:28:29 -0300

    Criação LogTransformado

---
+++ Core/Entities/TransformedLog.cs
@@ -0,0 +1,13 @@
+﻿using DevFreela.Core.Entities;
+using System;
+
+namespace LogTransformer.Core.Entities
+{
+    public class TransformedLog : BaseEntity
+    {
+        public string TransformedContent { get; set; }
+        public int OriginalLogId { get; set; }
+        public LogEntry OriginalLog { get; set; }
+    }
+
+}
