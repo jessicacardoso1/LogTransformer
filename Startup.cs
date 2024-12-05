2edb5fba02f9cbbedd53f0a81b9e8185f9350979
Jéssica Cardoso <jessica.cardosor@gmail.com>
2024-12-05 02:56:52 -0300

    Atualizações de configurações

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MediatR;
+using LogTransformer.Application.Commands.InsertLogEntry;
 
 namespace LogTransformer
 {
@@ -17,13 +19,10 @@ namespace LogTransformer
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Configura a versão da MVC para compatibilidade com o .NET Core 2.1
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            // Configuração do Swagger
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
@@ -33,12 +32,11 @@ namespace LogTransformer
                     Description = "API para transformação de logs"
                 });
             });
+            services.AddMediatR(typeof(InsertLogEntryCommand));
 
-            // Adiciona a infraestrutura (se você tiver implementações adicionais, como DbContext ou Repositories)
             services.AddInfrastructure(Configuration);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -50,20 +48,16 @@ namespace LogTransformer
                 app.UseHsts();
             }
 
-            // Usar o middleware Swagger
             app.UseSwagger();
 
-            // Configuração da UI do Swagger
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "LogTransformer API V1");
-                c.RoutePrefix = "swagger";  // Serve a UI do Swagger na raiz (opcional)
+                c.RoutePrefix = "swagger";
             });
 
-            // Adiciona suporte para requisições HTTPS (recomendado para produção)
             app.UseHttpsRedirection();
 
-            // Roteamento MVC
             app.UseMvc();
         }
     }
