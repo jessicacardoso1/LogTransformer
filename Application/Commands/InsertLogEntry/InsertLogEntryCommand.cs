ecc93dd296597171a56402f056466c897928d53a
Jéssica Cardoso <jessica.cardosor@gmail.com>
2024-12-05 21:46:36 -0300

    Criação Commands e Handler do Inserir Log

---
+++ Application/Commands/InsertLogEntry/InsertLogEntryCommand.cs
@@ -0,0 +1,17 @@
+﻿using LogTransformer.Application.Models;
+using LogTransformer.Core.Entities;
+using MediatR;
+
+namespace LogTransformer.Application.Commands.InsertLogEntry
+{
+    public class InsertLogEntryCommand : IRequest<ResultViewModel<int>>
+    {
+        public string OriginalContent { get; private set; }
+        public string SourceFileName { get; private set; }
+
+        public LogEntry ToEntity()
+        {
+            return new LogEntry(OriginalContent, SourceFileName);
+        }
+    }
+}
---
+++ Application/Commands/InsertLogEntry/InsertLogEntryHandler.cs
@@ -0,0 +1,24 @@
+﻿using LogTransformer.Application.Models;
+using LogTransformer.Core.Repositories;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LogTransformer.Application.Commands.InsertLogEntry
+{
+    public class InsertLogEntryHandler : IRequestHandler<InsertLogEntryCommand, ResultViewModel<int>>
+    {
+        private readonly ILogEntryRepository _repository;
+        public InsertLogEntryHandler(ILogEntryRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task<ResultViewModel<int>> Handle(InsertLogEntryCommand request, CancellationToken cancellationToken)
+        {
+            var logEntry = request.ToEntity();
+            await _repository.AddAsync(logEntry);
+
+            return ResultViewModel<int>.Success(logEntry.Id);
+        }
+    }
+}
