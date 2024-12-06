9f5b6af810fbea369de0e0d0cef941dde1d9c44e
Jéssica Cardoso <jessica.cardosor@gmail.com>
2024-12-05 21:46:08 -0300

    Criação interface file

---
+++ Core/Repositories/IFileService.cs
@@ -0,0 +1,15 @@
+﻿using LogTransformer.Core.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LogTransformer.Core.Repositories
+{
+    public interface IFileService
+    {
+        Task<string> SaveLogToFileAsync(TransformedLog transformedLog);
+        Task<string> DownloadLogAsync(string logUrl);
+        Task<string> ReadFileAsync(string filePath);
+        bool FileExists(string filePath);
+
+    }
+}
