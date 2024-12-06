24b46dc010d5fe05418eb3631c6fa5b6b3f47663
Jéssica Cardoso <jessica.cardosor@gmail.com>
2024-12-05 21:49:14 -0300

    Criação GetAllLog Handler e Query

---
+++ Application/Queries/GetAllLogsEntry/GetAllLogsEntryQuery.cs
@@ -0,0 +1,16 @@
+﻿using LogTransformer.Application.Models;
+using MediatR;
+using System.Collections.Generic;
+
+namespace LogTransformer.Application.Queries.GetAllLogsEntry
+{
+    public class GetAllLogsEntryQuery : IRequest<ResultViewModel<IEnumerable<LogEntryViewModel>>>
+    {
+        public GetAllLogsEntryQuery(string query)
+        {
+            Query = query;
+        }
+
+        public string Query { get; private set; }
+    }
+}
