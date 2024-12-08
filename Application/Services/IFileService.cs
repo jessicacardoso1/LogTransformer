ea974ef6a8b24192fc89adad14f8b66882da303f
Jéssica Cardoso <jessica.cardosor@gmail.com>
2024-12-08 03:10:52 -0300

    Alterando FileService

--- Application/Services/IFileService.cs
+++ Application/Services/IFileService.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace LogTransformer.Core.Repositories
+namespace LogTransformer.Application.Services
 {
     public interface IFileService
     {
-        Task<string> SaveLogToFileAsync(TransformedLog transformedLog);
+        Task<string> SaveLogToFileAsync(int OriginalLogId, string TransformedContent);
         Task<string> DownloadLogAsync(string logUrl);
         Task<string> ReadFileAsync(string filePath);
         bool FileExists(string filePath);
---
+++ Infrastructure/Services/FileService.cs
@@ -0,0 +1,58 @@
+﻿using LogTransformer.Application.Services;
+using LogTransformer.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace LogTransformer.Infrastructure.Services
+{
+    public class FileService : IFileService
+    {
+        private readonly HttpClient _httpClient;
+
+        public FileService(HttpClient httpClient)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        public async Task<string> SaveLogToFileAsync(int OriginalLogId, string TransformedContent)
+        {
+            var directoryPath = Path.Combine("Logs");
+            Directory.CreateDirectory(directoryPath);
+
+            var filePath = Path.Combine(directoryPath, $"log_{OriginalLogId}.txt");
+            await File.WriteAllTextAsync(filePath, TransformedContent);
+
+            return filePath;
+        }
+
+        public async Task<string> DownloadLogAsync(string logUrl)
+        {
+            try
+            {
+                var logContent = await _httpClient.GetStringAsync(logUrl);
+                return logContent;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Erro ao baixar o log da URL: {logUrl}. Detalhes: {ex.Message}");
+            }
+        }
+        public async Task<string> ReadFileAsync(string filePath)
+        {
+            if (!FileExists(filePath))
+            {
+                throw new FileNotFoundException($"Arquivo não encontrado: {filePath}");
+            }
+
+            return await File.ReadAllTextAsync(filePath);
+        }
+        public bool FileExists(string filePath)
+        {
+            return File.Exists(filePath);
+        }
+    }
+}
