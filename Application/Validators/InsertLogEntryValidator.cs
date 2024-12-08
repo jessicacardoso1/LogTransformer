05fe4cf790dd2c54a6455fe1689a6cc554d1c487
Jéssica Cardoso <jessica.cardosor@gmail.com>
2024-12-08 03:09:41 -0300

    Criando Validators

---
+++ Application/Validators/InsertLogEntryValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FluentValidation;
+using LogTransformer.Application.Commands.InsertLogEntry;
+using LogTransformer.Application.Commands.TransformLog;
+
+namespace LogTransformer.Application.Validators
+{
+    public class InsertLogEntryValidator : AbstractValidator<InsertLogEntryCommand>
+    {
+        public InsertLogEntryValidator()
+        {
+            RuleFor(x => x.OriginalContent)
+                .NotEmpty().WithMessage("O conteúdo do log não pode ser vazio.")
+                .MinimumLength(10).WithMessage("O conteúdo do log deve ter pelo menos 10 caracteres.");
+        }
+    }
+}
---
+++ Application/Validators/TransformLogValidators.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using LogTransformer.Application.Commands.TransformLog;
+using System;
+
+public class TransformLogCommandValidator : AbstractValidator<TransformLogCommand>
+{
+    public TransformLogCommandValidator()
+    {
+      
+        RuleFor(x => x.LogId)
+            .GreaterThan(0).When(x => x.LogId.HasValue)
+            .WithMessage("LogId deve ser maior que 0.");
+
+        RuleFor(x => x.LogUrl)
+            .NotEmpty().When(x => !x.LogId.HasValue) 
+            .WithMessage("LogUrl é obrigatório se LogId for informado.");
+
+
+        RuleFor(x => x.OutputFormat)
+            .NotEmpty()
+            .WithMessage("OutputFormat é obrigatório.")
+            .Must(format => format == "file" || format == "content")
+            .WithMessage("OutputFormat deve ser 'file' ou 'content'.");
+    }
+}
