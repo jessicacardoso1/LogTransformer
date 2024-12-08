376e33a0184bf06365c46993915da97290a6b94b
Jéssica Cardoso <jessica.cardosor@gmail.com>
2024-12-08 03:12:19 -0300

    Alterações Swagger

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,17 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MediatR;
 using LogTransformer.Application.Commands.InsertLogEntry;
+using LogTransformer.Application.Models;
+using LogTransformer.Application.Commands.TransformLog;
+using FluentValidation.AspNetCore;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerUI;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+
+
 
 namespace LogTransformer
 {
@@ -21,21 +32,33 @@ namespace LogTransformer
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvcCore()
+                     .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                     .AddApiExplorer()
+                     .AddJsonOptions(options =>
+                     {
+                         options.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+                     });
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                c.SwaggerDoc("v1", new Info
                 {
                     Title = "LogTransformer API",
                     Version = "v1",
                     Description = "API para transformação de logs"
                 });
             });
+
             services.AddMediatR(typeof(InsertLogEntryCommand));
 
+            services.AddMvcCore().AddFluentValidation(fv => { fv.RegisterValidatorsFromAssemblyContaining<Startup>(); });
             services.AddInfrastructure(Configuration);
+            services.AddMvcCore().AddJsonFormatters().AddApiExplorer();
+
         }
+
+
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
@@ -48,15 +71,17 @@ namespace LogTransformer
                 app.UseHsts();
             }
 
+            //app.UseStaticFiles(); // Adicione esta linha para servir arquivos estáticos
+
             app.UseSwagger();
 
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "LogTransformer API V1");
-                c.RoutePrefix = "swagger";
+                c.RoutePrefix = string.Empty; /
             });
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseMvc();
         }
